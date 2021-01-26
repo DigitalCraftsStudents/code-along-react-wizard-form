1bff48cb0d841277e0ecbe07f8ce2545499da192
Chris Aquino <chris@radishmouse.com>
2021-01-26 15:05:17 -0500

    adds occupation field

--- src/components/WizardForm.jsx
+++ src/components/WizardForm.jsx
@@ -6,18 +6,47 @@ function WizardForm(props) {
     // 1st value is always your state variable
     // 2nd value is always the setState function (for updating the variable)
     const [name, setName] = useState('');
-
+    const [occupation, setOccupation] = useState('');
+    const [house, setHouse] = useState('');
     return (
         <section>
             <h1>Wizard Form</h1>
             <form>
-                <input 
-                    value={name} 
-                    onChange={(e) => {
-                        console.log(e.target.value);
-                        setName(e.target.value);
-                    }}
-                />
+                <label>
+                    Name:
+                    <input 
+                        placeholder="Ron Weasley"
+                        value={name} 
+                        onChange={(e) => {
+                            console.log(e.target.value);
+                            setName(e.target.value);
+                        }}
+                    />
+                </label>
+                <br />
+                <label>
+                    Occupation:
+                    <input 
+                        placeholder="Auror"
+                        value={occupation}
+                        onChange={(e) => {
+                            console.log(e.target.value);
+                            setOccupation(e.target.value);
+                        }}
+                    />
+                </label>
+                <br />
+                <label>
+                    House:
+                    <input 
+                        placeholder="Hufflepuff"
+                        value={house}
+                        onChange={(e) => {
+                            console.log(e.target.value);
+                            setHouse(e.target.value);
+                        }}
+                    />
+                </label>
             </form>
         </section>
     );
